2ffe1080c5fa0d028594df0cfdd8fc4f00ea1386
Devanshu Kushwah <devanshukushwah.001@gmail.com>
2024-11-03 21:30:12 +0530

    junit test cases for UserServiceImplTest.java

--- src/test/java/com/checklist/service/impl/UserServiceImplTest.java
+++ src/test/java/com/checklist/service/impl/UserServiceImplTest.java
@@ -1,5 +1,7 @@
 package com.checklist.service.impl;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
@@ -14,6 +16,8 @@ import org.mockito.MockitoAnnotations;
 import com.checklist.DAO.UserDAO;
 import com.checklist.exception.DatabaseError;
 import com.checklist.exception.UserAlreadyExists;
+import com.checklist.exception.UserNotFound;
+import com.checklist.exception.UserPasswordIncorrect;
 import com.checklist.model.User;
 
 class UserServiceImplTest {
@@ -33,6 +37,8 @@ class UserServiceImplTest {
 	private static final int INVALID_USER_ID = -1;
 	private static final String VALID_EMAIL = "valid@valid.valid";
 	private static final String VALID_PASSWORD = "ValidPassword";
+	private static final String INVALID_EMAIL = "invalid@invalid.invalid";
+	private static final String INVALID_PASSWORD = "InvalidPassword";
 
 	@Test
 	@DisplayName("should create user when user not exist")
@@ -52,7 +58,7 @@ class UserServiceImplTest {
 	}
 	
 	@Test
-	@DisplayName("should not create user when user already exist")
+	@DisplayName("should throw UserAlreadyExists exception when user already exist")
 	void testNotCreateUser() throws DatabaseError, UserAlreadyExists {
 		// Given
 		User user = new User();
@@ -65,6 +71,52 @@ class UserServiceImplTest {
 		// Then
 		assertThrows(UserAlreadyExists.class, () -> {
 			userServiceImpl.create(user);
+		});
+	}
+	
+	@Test
+	@DisplayName("should return user when successfully login")
+	void testGetUserForSuccessfullyLogin() throws DatabaseError, UserNotFound, UserPasswordIncorrect {
+		// Given
+		User user = new User();
+		user.setEmail(VALID_EMAIL);
+		user.setPassword(VALID_PASSWORD);
+		
+		// When
+		when(userDAO.userExists(VALID_EMAIL)).thenReturn(true);
+		when(userDAO.getUser(VALID_EMAIL, VALID_PASSWORD)).thenReturn(user);
+		
+		// Then
+		User res = userServiceImpl.getUser(VALID_EMAIL, VALID_PASSWORD);
+		assertNotNull(res);
+		assertEquals(user.getEmail(), res.getEmail());	
+	}
+	
+	@Test
+	@DisplayName("should throw UserNotFound exception when user not exists")
+	void testGetUserForUserNotFound() throws DatabaseError {
+		// When
+		when(userDAO.userExists(INVALID_EMAIL)).thenReturn(false);
+		
+		// Then
+		assertThrows(UserNotFound.class, () -> {
+			userServiceImpl.getUser(INVALID_EMAIL, INVALID_PASSWORD);
+		});
+	}
+	
+	@Test
+	void testGetUserForPasswordIncorrect() throws DatabaseError, UserNotFound {
+		// Given
+		User user = new User();
+		user.setEmail(VALID_EMAIL);
+		user.setPassword(VALID_PASSWORD);
+		
+		// When
+		when(userDAO.userExists(VALID_EMAIL)).thenReturn(true);
+		
+		// Then
+		assertThrows(UserPasswordIncorrect.class, () -> {
+			userServiceImpl.getUser(VALID_EMAIL, INVALID_PASSWORD);
 		});
 	}
 
